f1bb39cbc46bc2317222de20c914967d5dba09c4
manvigautam09 <mnvsgautam9@gmail.com>
2025-02-13 12:56:35 +0530

    Adds progress bar

---
+++ Progress/progress.js
@@ -0,0 +1,61 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const parent = document.getElementById("app");
+
+  const progressDiv = document.createElement("div");
+  progressDiv.classList.add("progress");
+  parent.appendChild(progressDiv);
+
+  const progressBar = document.createElement("div");
+  progressBar.classList.add("progress-bar");
+  progressDiv.appendChild(progressBar);
+
+  progressBar.setAttribute("aria-label", "Progress Bar");
+  progressBar.setAttribute("role", "progressBar");
+  progressBar.setAttribute("aria-valuenow", "0");
+  progressBar.setAttribute("aria-valuemin", "0");
+  progressBar.setAttribute("aria-valuemax", "100");
+
+  let intervalId = null;
+
+  const startProgressBar = () => {
+    clearInterval(intervalId);
+    intervalId = setInterval(() => {
+      const width = parseInt(progressBar.style.width) || 0;
+      if (width + 1 >= 100) {
+        clearInterval(intervalId);
+        intervalId = null;
+      }
+      progressBar.style.width = `${width + 1}%`;
+    }, 100);
+  };
+
+  const pauseInterval = () => {
+    if (intervalId) {
+      clearInterval(intervalId);
+      intervalId = null;
+    }
+  };
+
+  const stopInterval = () => {
+    if (parseInt(progressBar.style.width) > 0) {
+      clearInterval(intervalId);
+      intervalId = null;
+      progressBar.style.width = "0%";
+    }
+  };
+
+  const buttonStart = document.createElement("button");
+  buttonStart.textContent = "Start";
+  buttonStart.onclick = startProgressBar;
+  parent.appendChild(buttonStart);
+
+  const buttonPause = document.createElement("button");
+  buttonPause.textContent = "Pause";
+  buttonPause.addEventListener("click", pauseInterval);
+  parent.appendChild(buttonPause);
+
+  const buttonStop = document.createElement("button");
+  buttonStop.textContent = "Stop";
+  parent.appendChild(buttonStop);
+  buttonStop.addEventListener("click", stopInterval);
+});
